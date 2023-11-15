c89ed1b9eea00108978fdd0f4bcd4682cd71c8ed
Yian Wang <wangyian@patas.ling.washington.edu>
2023-11-14 19:25:24 -0800

    cbc solver version

--- workspace/run.sh
+++ workspace/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
+source activate /projects/assigned/course-scheduling/Course-Scheduling-System/env
 output_dir="/projects/assigned/course-scheduling/Course-Scheduling-System/workspace/output"
 if [ ! -d "$output_dir" ]; then
     mkdir -p "$output_dir"
 fi
-/projects/assigned/course-scheduling/Course-Scheduling-System/env/bin/python3 ../bin/time-schedule.py config 1>output/log.stdout 2>output/log.stderr
+#/projects/assigned/course-scheduling/Course-Scheduling-System/env/bin/python3 ../bin/time-schedule.py config 1>output/log.stdout 2>output/log.stderr
+python3 ../bin/time-schedule.py config 1>output/log.stdout 2>output/log.stderr
 cp CoursesThisQuarter output/
 cp config output/
 cp ConflictCourses output/
